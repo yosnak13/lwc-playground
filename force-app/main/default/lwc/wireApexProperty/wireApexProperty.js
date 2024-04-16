55bcb7be84ecd2f3eedd1e9ab1a5a98410731afb
yosnak13 <ynakai@circlace.com>
2024-04-16 10:21:00 +0900

    delete unnecessary components

--- force-app/main/default/lwc/wireApexProperty/wireApexProperty.js
+++
@@ -1,8 +0,0 @@
-import {LightningElement, api, wire} from 'lwc';
-import getContactsBornAfter from '@salesforce/apex/ContactController.getContactsBornAfter';
-
-export default class WireApexProperty extends LightningElement {
-    @api minBirthDate;
-    @wire(getContactsBornAfter, {birthDate: '$minBirthDate'})
-    contacts;
-}
