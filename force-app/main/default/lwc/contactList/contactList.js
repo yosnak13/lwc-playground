76c33f949b320543fb5015c9463b9717c6139e18
yosnak13 <ynakai@circlace.com>
2024-04-16 11:58:05 +0900

    fix getContacts like normalization

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -7,15 +7,20 @@ import EMAIL from '@salesforce/schema/Contact.Email';
 import LAST_NAME from '@salesforce/schema/Contact.LastName';
 import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import {reduceErrors} from "c/ldsUtils";
 
 const COLUMNS = [
-    {label: 'FirstName', fieldName: FIRST_NAME.fieldApiName, type: 'text'},
-    {label: 'LastName', fieldName: LAST_NAME.fieldApiName, type: 'text'},
-    {label: 'Email', fieldName: EMAIL.fieldApiName, type: 'text'}
+  {label: 'FirstName', fieldName: FIRST_NAME.fieldApiName, type: 'text'},
+  {label: 'LastName', fieldName: LAST_NAME.fieldApiName, type: 'text'},
+  {label: 'Email', fieldName: EMAIL.fieldApiName, type: 'text'}
 ];
 
 export default class ContactList extends LightningElement {
-    columns = COLUMNS;
-    @wire(getContacts)
-    contacts;
+  columns = COLUMNS;
+  @wire(getContacts)
+  contacts;
+
+  get errors() {
+    return (this.contacts.error) ? reduceErrors(this.contacts.error) : [];
+  }
 }
