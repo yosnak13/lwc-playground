3151164261c8afd8950a39a84fc46f9e8bbc88a2
yosnak13 <ynakai@circlace.com>
2024-04-17 10:27:40 +0900

    create unit test for unitTestClass

--- force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++ force-app/main/default/lwc/unitTest/__tests__/sum.test.js
@@ -1,7 +1,7 @@
 import { sum } from '../sum';
 
-describe('sum()', () => {
-  it('should add 1 and 2 returning 3', () => {
+describe('sum()', () => { // 通常第一引数は名詞
+  it('should add 1 and 2 returning 3', () => { // // 通常第二引数は動詞
     expect(sum(1, 2)).toBe(3);
   });
 });
--- force-app/main/default/lwc/unitTest/unitTest.js
+++ force-app/main/default/lwc/unitTest/unitTest.js
@@ -2,8 +2,9 @@
  * Created by yoshihisanakai on 2024/04/17.
  */
 
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
+import {sum} from './sum';
 
 export default class UnitTest extends LightningElement {
-
+  @api unitNumber = sum(2, 3);
 }
