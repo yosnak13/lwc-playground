8ae33f3772fd74c08fa55dd36126fcbfe5caca47
yosnak13 <ynakai@circlace.com>
2024-04-17 11:00:03 +0900

    add test suite that change property by unit test

--- force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -20,4 +20,42 @@ describe('c-unit-test', () => {
     const div = element.shadowRoot.querySelector('div');
     expect(div.textContent).toBe('Unit 5 alive!');
   });
+
+  it('displays unit status with updated unitNumber', () => {
+    const element = createElement('c-unit-test', {
+      is: UnitTest
+    });
+    // Add the element to the jsdom instance
+    document.body.appendChild(element);
+    // Update unitNumber after element is appended
+    element.unitNumber = 6
+
+    const div = element.shadowRoot.querySelector('div');
+    // Verify displayed unit status
+    expect(div.textContent).not.toBe('Unit 6 alive!');
+
+    // Return a promise to wait for any asynchronous DOM updates. Jest
+    // will automatically wait for the Promise chain to complete before
+    // ending the test and fail the test if the promise rejects.
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe('Unit 6 alive!');
+    });
+  });
+
+  it('displays unit status with input change event', () => {
+    const element = createElement('c-unit-test', {
+      is: UnitTest
+    });
+    document.body.appendChild(element);
+    const div = element.shadowRoot.querySelector('div');
+
+    // Trigger unit status input change
+    const inputElement = element.shadowRoot.querySelector('lightning-input');
+    inputElement.value = 7;
+    inputElement.dispatchEvent(new CustomEvent('change'));
+
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe('Unit 7 alive!');
+    });
+  });
 });
