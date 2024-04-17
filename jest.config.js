6b5e204af13dbe18829fe4e5eef460ea396b7819
yosnak13 <ynakai@circlace.com>
2024-04-17 15:28:26 +0900

    add override stub

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
+    moduleNameMapper: {
+        '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton',
+        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+    },
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
