f1960c6f44f31668e5ab8dbc2b3349f616b29808
yosnak13 <ynakai@circlace.com>
2024-04-17 11:00:18 +0900

    add test suite that change property by input-tag

--- force-app/main/default/lwc/unitTest/unitTest.js
+++ force-app/main/default/lwc/unitTest/unitTest.js
@@ -7,4 +7,8 @@ import {sum} from './sum';
 
 export default class UnitTest extends LightningElement {
   @api unitNumber = sum(2, 3);
+
+  handleChange(event) {
+    this.unitNumber = event.target.value;
+  }
 }
