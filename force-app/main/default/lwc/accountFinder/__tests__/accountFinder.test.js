b811220ce7d12ab302a404f1543f54883db2b165
yosnak13 <ynakai@circlace.com>
2024-04-19 14:39:19 +0900

    fix lwc accountFinder to get accounts by  apex class

--- force-app/main/default/lwc/accountFinder/__tests__/accountFinder.test.js
+++ force-app/main/default/lwc/accountFinder/__tests__/accountFinder.test.js
@@ -1,12 +1,30 @@
 import {createElement} from 'lwc';
 import accountFinder from 'c/accountFinder';
-import * as events from "node:events";
+import {createApexTestWireAdapter} from "@salesforce/sfdx-lwc-jest";
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
+
+const mockAccountsByRevenue = require('./data/queryAccountsByRevenue.json');
+
+jest.mock(
+  '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue',
+  () => {
+    const {
+      createApexTestWireAdapter
+    } = require('@salesforce/sfdx-lwc-jest');
+    return {
+      default: createApexTestWireAdapter(jest.fn())
+    };
+  },
+  {virtual: true}
+);
+
 
 describe('c-account-finder', () => {
   afterEach(() => {
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
     }
+    jest.clearAllMocks();
   });
 
   it('sets annualRevenue when handleChange is called', () => {
@@ -36,5 +54,31 @@ describe('c-account-finder', () => {
     const resetEvent = new CustomEvent('reset');
     element.dispatchEvent(resetEvent);
     expect(element.annualRevenue).toBeNull;
+  });
+
+  it('getAccounts @wire queryAccountsByRevenue', () => {
+    const element = createElement('c-account-finder', {
+      is: accountFinder
+    });
+    document.body.appendChild(element);
+    queryAccountsByRevenue.emit(mockAccountsByRevenue);
+
+    return Promise.resolve().then(() => {
+      const accountElements = element.shadowRoot.querySelectorAll('p');
+      expect(accountElements.length).toBe(mockAccountsByRevenue.length);
+    });
+  });
+
+  it('error @wire queryAccountsByRevenue', () => {
+    const element = createElement('c-account-finder', {
+      is: accountFinder
+    });
+    document.body.appendChild(element);
+    queryAccountsByRevenue.error();
+
+    return Promise.resolve().then(() => {
+      const errorElement = element.shadowRoot.querySelectorAll('p');
+      expect(errorElement).not.toBeNull();
+    });
   });
 });
--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -3,7 +3,8 @@
  * https://trailhead.salesforce.com/ja/content/learn/modules/lwc-for-visualforce-developers/handle-user-actions-in-javascript?trail_id=build-lightning-web-components
  */
 
-import {api, LightningElement} from 'lwc';
+import {api, LightningElement, wire} from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 
 export default class AccountFinder extends LightningElement {
   @api annualRevenue = null;
@@ -15,4 +16,7 @@ export default class AccountFinder extends LightningElement {
   reset() {
     this.annualRevenue = null;
   }
+
+  @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue'})
+  accounts;
 }
