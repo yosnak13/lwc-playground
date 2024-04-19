9cc2a0693f10d4200a7939178f12d522cef04ec7
yosnak13 <ynakai@circlace.com>
2024-04-19 09:24:01 +0900

    create LWC accountSearch

---
+++ force-app/main/default/lwc/accountSearch/accountSearch.js
@@ -0,0 +1,18 @@
+/**
+ * Created by yoshihisanakai on 2024/04/19.
+ * https://trailhead.salesforce.com/ja/content/learn/modules/lwc-for-visualforce-developers/handle-user-actions-in-javascript?trail_id=build-lightning-web-components
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class AccountSearch extends LightningElement {
+  numberOfEmployees = null;
+
+  handleChange(event) {
+    this.numberOfEmployees = event.detail.value;
+  }
+
+  reset() {
+    this.numberOfEmployees = null;
+  }
+}
